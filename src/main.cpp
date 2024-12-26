8dc4e4209f3f78f081d3d760a80c0639030cab28
Remy Fayet <remy.fayet@telecom-paris.fr>
2024-12-26 17:41:59 +0100

    base commit

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <cstdlib>
+
+// clang-format off
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+// clang-format on
+
+void init() {}
+
+int main(int argc, char **argv) {
+    init();
+    return EXIT_SUCCESS;
+}
