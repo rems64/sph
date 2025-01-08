2e61bdf4dc5c2bbc479b1d9de046033759df5476
Remy Fayet <remy.fayet@telecom-paris.fr>
2025-01-08 14:33:28 +0100

    fix kernels derivatives

--- src/Kernels.hpp
+++ src/Kernels.hpp
@@ -35,9 +35,9 @@ static real_t base_near_kernel(real_t dst, real_t radius) {
 
 static real_t base_near_kernel_derivative(real_t dst, real_t radius) {
     if (dst <= radius) {
-        float scale = 45 / (pow(radius, 6) * M_PI);
+        float scale = 2.f / (M_PI * M_PI * powf(radius, 4));
         float v = radius - dst;
-        return -v * v * scale;
+        return -3.f * v * v * scale;
     }
     return 0;
 }
@@ -58,9 +58,9 @@ static real_t cubic_spline_kernel_derivative(real_t r, real_t h) {
     const real_t q = r / h;
     const real_t alpha3 = 3.f / (2.f * M_PI * h * h * h);
     if (q >= 0 && q < 1) {
-        return alpha3 * (-2.f * q + 3.f * 0.5f * q * q);
+        return alpha3 * (-2.f * q / h + 3.f * 0.5f * q * q / h / h);
     } else if (q >= 1 && q < 2) {
-        return alpha3 * (-1.f / 2.f) * (2.f - q) * (2.f - q);
+        return alpha3 * (-1.f / 2.f) * (2.f - q / h) * (2.f - q / h);
     } else {
         return 0;
     }
